a6f161c66ac46483767a78af78084a04651caf20
Axel HEINE <axheine@gmail.com>
2017-02-04 21:45:10 +0100

    calendrier: css & methode de génération

--- imports/ui/pages/agendaMonth.js
+++ imports/ui/pages/agendaMonth.js
@@ -1,6 +1,9 @@
 
 
 import './agendaMonth.html';
+import './agendaMonth.css';
+
+import '../components/dayBlock.js';
 
 
 Template.agendaMonth.helpers({
@@ -14,7 +17,7 @@ Template.agendaMonth.helpers({
 	previousMonthLink: function(year, month){
 		let newYear = parseInt(year);
 		let newMonth = parseInt(month)-1;
-		
+
 		if(newMonth == 0) {
 			newYear = newYear-1;
 			newMonth = 12;
@@ -26,7 +29,7 @@ Template.agendaMonth.helpers({
 	nextMonthLink: function(year, month){
 		let newYear = parseInt(year);
 		let newMonth = parseInt(month)+1;
-		
+
 		if(newMonth == 13) {
 			newYear = newYear+1;
 			newMonth = 1;
@@ -51,11 +54,64 @@ Template.agendaMonth.helpers({
 	},
 
 	buildPreviousMonthDays: function(days) {
+		console.log("call");
 		let firstDay = (days[0].getDay()+6)%7;
 		let blocs = "";
 		for(let i=0; i<firstDay; i++) {
 			blocs = blocs + "<td></td>";
 		}
 		return blocs;
+	},
+
+	isSunday: function(day) {
+		console.log(day+" is sunday? "+(day.getDay() == 0));
+		return (day.getDay() === 0 ? "<!-- end --></tr><tr>" : "");
+	},
+
+	buildWeek: function(weekNumber, days) {
+		console.log("WEEK NUMBER "+weekNumber);
+
+		let output = "";
+		let firstDay = days[0];
+		let daysNumberBeforeMonth = (firstDay.getDay()+6)%7;
+
+		console.log(daysNumberBeforeMonth+" before beginning of the month");
+
+		// Fill beginning with blanks
+		if(weekNumber === 0) {
+			for(let i=0; i<daysNumberBeforeMonth; i++) {
+				output = output + "<td></td>";
+			}
+		}
+
+		let daysToCount = daysNumberBeforeMonth+days.length;
+		let beginningOfWeek = 7*weekNumber;
+		let daysToWrite = 7;
+
+		if(beginningOfWeek < daysNumberBeforeMonth) {
+			beginningOfWeek += daysNumberBeforeMonth;
+			daysToWrite -= beginningOfWeek;
+		}
+
+		console.log(daysToCount+" existing, beginning of this week is in position "+beginningOfWeek+". Will write "+daysToWrite+" days");
+
+		// Fill end with blanks
+		if(weekNumber > 0) {
+			if(days.length - (beginningOfWeek - daysNumberBeforeMonth) < 7) {
+				console.log("end of month, "+(days.length-(beginningOfWeek- daysNumberBeforeMonth ))+" "+days.length+" "+beginningOfWeek);
+				daysToWrite = days.length - (beginningOfWeek - daysNumberBeforeMonth);
+			}
+		}
+		let year = Router.current().params.year;
+		let month = Router.current().params.month;
+
+		for(let i = beginningOfWeek; i < beginningOfWeek+daysToWrite; i++) {
+			let day = days[i-daysNumberBeforeMonth].getDate();
+			output += "<td><a href='/agenda/"+year+"/"+month+"/"+day+"/'>"
+				+ "<div class='dayNumber'>"+day+"</div>"
+				+ "</a></td>";
+		}
+
+		return output;
 	}
 });
