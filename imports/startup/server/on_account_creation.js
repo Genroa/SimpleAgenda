8dceb6b60cca9c7244d281d256f942e77b344a87
Genroa <genroa@hotmail.fr>
2017-03-09 20:51:31 +0100

    Work on default colors + agendaDay scrolling fix

--- imports/startup/server/on_account_creation.js
+++ imports/startup/server/on_account_creation.js
@@ -8,7 +8,13 @@ Accounts.onCreateUser(function(options, user) {
 	user.adsViewCount = 0;
 	
 	user.maxColors = 5;
-	user.definedColors = {};
+	user.definedColors = {
+		0: "#000000",
+		1: "#000000",
+		2: "#000000",
+		3: "#000000",
+		4: "#000000"
+	};
 
 	return user;
 });
